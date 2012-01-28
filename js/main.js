2c1895b45ebf440f5634c12e803b2f1da6f85a76
Adam Powers <a2powers@ucsd.edu>
2012-01-28 01:26:05 -0800

    Just a tiny bit late.... be gentle. ;)

--- js/main.js
+++ js/main.js
@@ -1,30 +1,10 @@
 $(document).ready(function() {
-
-  /*
-    Simple image gallery. Use default settings
-  */
-  $('.fancybox').fancybox();
-
-
-$(document).ready(sizeContent);
-$(window).resize(sizeContent);
-
-//Dynamically assign height
-function sizeContent() {
-    var newHeight = $("html").height() + "px";
-    $("#sidebar").css("height", newHeight);
-}
-
-});
-
-//Initial load of page
-
-
-//Every resize of window
-$(window).resize(sizeContent);
-
-//Dynamically assign height
-function sizeContent() {
-    var newHeight = $("html").height() + "px";
-    $("#wrapper").css("height", newHeight);
-}
+    
+    $('.fancybox').fancybox(
+        {
+        'width' : '100%',
+        'height' : '100%',
+        'autoScale' : true,
+        'type'    :  'iframe'               
+        });
+    });
